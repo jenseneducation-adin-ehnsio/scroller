9ec6612a7f15c2758f50b26622c7a76e733af5d9
jenseneducation-adin-ehnsio <55272975+jenseneducation-adin-ehnsio@users.noreply.github.com>
2021-01-31 12:54:12 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="progress_bar"></div>
+  <div @click="animate" :class="{ bigger: cubeAnimate }" class="cube-wrap">
+    <div class="cube">
+      <div class="side top"><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6></div>
+      <div class="side bottom"><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6></div>
+      <div class="side front"><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6></div>
+      <div class="side back"><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6></div>
+      <div class="side left"><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6></div>
+      <div class="side right"><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6><h6>afonso</h6></div>
+    </div>
   </div>
   <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+<script>
 
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: "App",
+
+  data() {
+    return {
+      cubeAnimate: false
     }
-  }
-}
+  },
+
+  created() {
+    document.body.style.setProperty('--scroll', 0);
+    window.addEventListener('scroll', () => {
+      document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+    }, false);
+  },
+
+  methods: {
+    animate() {
+      this.cubeAnimate = true
+      setTimeout(() => {
+        this.cubeAnimate = false
+      }, 500);
+    }
+  },
+
+};
+</script>
+
+<style lang="scss">
+@import "@/styles/index";
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main class="home">
+    <header >
+      <h1 class="first" :class="{ fade: visible.first }">AFONSO</h1>
+    </header>
+    <section>
+      <h2 class="second" :class="{ fade_up: visible.second }">Vi utvecklar våra kunder digitalt!</h2>
+    </section>
+    <footer class="switch">
+      <h2 class="third" :class="{ fade_up: visible.third }">Color change yall</h2>
+    </footer>
+    <section></section>
+    <section></section>
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+
+  data() {
+    return {
+      visible: {
+        first: true,
+        second: false,
+        third: false
+      },
+      scrollBefore: 0
+    }
+  },
+
+  created() {
+    document.addEventListener("scroll", this.animate)
+  },
+
+  methods: {
+    animate() {
+      setTimeout(() => {
+          // if(this.inView(document.querySelector(".switch"))) {
+          //   document.body.classList.add('bg')
+          // }
+          // else {
+          //   document.body.classList.remove('bg')
+          // }
+          const scrolled = window.scrollY;
+          if(this.scrollBefore < scrolled) {
+            this.scrollBefore = scrolled;
+            this.visible.first = this.inView(document.querySelector(".first"));
+            this.visible.second = this.inView(document.querySelector(".second"));
+            this.visible.third = this.inView(document.querySelector(".third"));
+          }
+        }, 100);
+    },
+    inView(el) {
+      let windowHeight = window.innerHeight;
+      let scrollY = window.scrollY || window.pageYOffset;
+      let scrollPosition = scrollY + windowHeight/1.2;
+      let elementPosition = el.getBoundingClientRect().top + scrollY + el.clientHeight;
+      if(scrollPosition > elementPosition) return true
+      else return false
+    }
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+
+</style>
